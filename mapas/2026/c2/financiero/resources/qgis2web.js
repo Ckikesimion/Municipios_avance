00d93a84435b71b64a8901d120b8f9c459dbd1ac
Ckikesimion <104583175+Ckikesimion@users.noreply.github.com>
2026-09-18 13:46:21 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- mapas/2026/c2/financiero/resources/qgis2web.js
+++ mapas/2026/c2/financiero/resources/qgis2web.js
@@ -13,8 +13,6 @@ var map = new ol.Map({
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
 map.getView().fit([-10404547.272172, 1581537.322140, -9595917.050325, 2044320.324974], map.getSize());
-   
-);
 
 //change cursor
 function pointerOnFeature(evt) {
