f60b0db99f4eb797697d6b72d1aaa9c363a6d017
Ckikesimion <104583175+Ckikesimion@users.noreply.github.com>
2026-09-18 11:01:08 -0600

    Update qgis2web.js

--- mapas/2026/c2/financiero/resources/qgis2web.js
+++ mapas/2026/c2/financiero/resources/qgis2web.js
@@ -12,7 +12,13 @@ var map = new ol.Map({
 });
 
 //initial view - epsg:3857 coordinates if not "Match project CRS"
-map.getView().fit([-10404547.272172, 1581537.322140, -9595917.050325, 2044320.324974], map.getSize());
+map.getView().fit(
+    [-10404547.272172, 1581537.322140, -9595917.050325, 2044320.324974],
+    {
+        size: map.getSize(),
+        padding: [35, 35, 35, 35]
+    }
+);
 
 //change cursor
 function pointerOnFeature(evt) {
